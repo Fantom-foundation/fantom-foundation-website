e9c6df7a1c162a5dcf43f6d5e384296d0fb73b45
Amit <amit.kumar@catalyst.sh>
2020-04-07 18:58:12 +0530

    Complete blog pages and responsiveness

--- single.php
+++ single.php
@@ -6,91 +6,136 @@
 get_header();
 $feat_image = wp_get_attachment_url(get_post_thumbnail_id(get_the_id()));
 while (have_posts()) : the_post();
-    ?>
-    <main class="post-sec-page">
-        <div class="container">
-            <div class="row post-page-row">
-                <div class="col-sm-8 first-col-sec">
-                    <div class="fullwidth banner-section single-wrapper">
-                        <div class="blog-banner-wrapper">
-                            <div class="post-img-sec">
-                                <img src="<?php echo $feat_image; ?>" class="post-feat-image" alt="blog-image">
-                            </div> 
-                            <h1><?php the_title() ?></h1>
-                            <div class="post-detail">
-                                <div class="post-date">
-                                    <i class="far fa-clock"></i><?php echo the_time('F j, Y'); ?>
-                                </div> 
-                                <div class="post-author">
-                                    <i class="far fa-user"></i><?php echo get_author_name(); ?>                              
-                                </div>                          
-                            </div>
-                            <div class="post-content">
-                                <?php the_content() ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4 right-sec">
-                    <section class="related-posts-section">
-                        <div class="container">
-                            <?php get_search_form(); ?>
-                            <h4>Related Posts</h4>
-                            <div class="related-posts-row">
-                                <?php
-                                $i = 1;
-                                $terms = get_the_terms(get_the_ID(), 'category');
-                                $term_list = wp_list_pluck($terms, 'slug');
-                                $related_args = array(
-                                    'post_type' => 'post',
-                                    'posts_per_page' => -1,
-                                    'post_status' => 'publish',
-                                    'post__not_in' => array(get_the_ID()),
-                                    'orderby' => 'rand',
-                                    'tax_query' => array(
-                                        array(
-                                            'taxonomy' => 'category',
-                                            'field' => 'slug',
-                                            'terms' => $term_list
-                                        )
-                                    )
-                                );
-                                $loop = new WP_Query($related_args);
-                                if ($loop->have_posts()) :
-                                    while ($loop->have_posts()) : $loop->the_post();
-                                        $featimages = wp_get_attachment_url(get_post_thumbnail_id(get_the_id()));
-                                        ?>
-                                        <div class="post-wrapper" <?php echo $i; ?>s">
-                                             <a class="post-link" href="<?php the_permalink(); ?>">
-                                                <p><?php the_title(); ?><p>                             
-                                            </a>
-                                        </div>
-                                        <?php
-                                        $i++;
-                                    endwhile;
-                                endif;
-                                wp_reset_postdata();
-                                ?>
-                            </div>
-                            <div class="categories-section-wrapper">
-                                  <h4>CATEGORIES</h4>
-                                <?php
-                                wp_list_categories(array(
-                                    'orderby' => 'name',
-                                    'title_li' => ''
-                                ));
-                                ?> 
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </div>
+  ?>
+  <main class="post-sec-page blog-page-sec-wrapper">
+      <div class="container">
+          <div class="post-page-row">     
+              <div class="blog-banner-wrapper">
+                  <div class="post-img-sec">
+                      <img src="<?php echo $feat_image; ?>" class="post-feat-image" alt="blog-image">
+                  </div> 
+                  <div class="blog-detail-section">
+                      <div class="post-date">
+                          <?php echo the_time('F j, Y'); ?>
+                      </div>
+                      <div class="row">
+                          <div class="col-sm-2">
+                              <div class="social-icon-wrapper">
+                                  <a href="https://twitter.com/FantomFDN" target="_blank" class="social-link">
+                                      <img src="<?php echo get_template_directory_uri(); ?>/images/twitter.svg" class="social-icon-image" alt="Twitter">  
+                                  </a>                                
+                                  <a href="https://www.linkedin.com/company/fantom-foundation/" target="_blank" class="social-link">
+                                      <img src="<?php echo get_template_directory_uri(); ?>/images/linkedin.svg" class="social-icon-image" alt="Linkedin"> 
+                                  </a>                              
+                                  <a href="https://www.facebook.com/Fantom.Foundation.English/" target="_blank" class="social-link">
+                                      <img src="<?php echo get_template_directory_uri(); ?>/images/facebook.svg" class="social-icon-image" alt="Facebook">  
+                                  </a>
 
+                              </div>
+                          </div>
+                          <div class="col-sm-10">
+                              <div class="blog-detail-wrapper">                
 
+                                  <div class="post-content">
+                                      <h1><?php the_title() ?></h1> 
+                                      <div class="category-name">
+                                          <span class="category-name-wrapper"><?php the_category(', '); ?></span> 
+                                      </div>
+                                      <?php the_content() ?>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
 
-    </main>
-    <?php
+              </div>  
+              <div class="read-more-blog-sec">
+                  <h2>Read more</h2>
+                  <div class="medium-blog-row wrapper-blog-sec">          
+                      <?php
+                      $args = array(
+                          'post_status' => 'publish',
+                          'posts_per_page' => 3,
+                          'post__not_in' => array(get_the_id())
+                      );
+                      $testimonials = new WP_Query($args);
+                      if ($testimonials->have_posts()) :
+                        ?>
+                        <?php
+                        while ($testimonials->have_posts()) :
+                          $testimonials->the_post();
+                          ?>
+                          <div class="medium-blog-col blog-col-section">
+                              <a href="<?php the_permalink(); ?>" class="card-link">
+                                  <div class="card">									
+                                      <img class="card-img-top" src="<?php the_post_thumbnail_url($size); ?>" alt="Card image cap">
+                                      <div class="card-body">
+                                          <h5 class="card-title"><?php the_title(); ?></h5>
+                                          <div class="read-story-btn-wrapper">              
+                                              <span>READ STORY</span>
+                                          </div>                                         
+                                      </div>							
+                                  </div>
+                              </a>
+                          </div>
+                          <?php
+                        endwhile;
+                        wp_reset_postdata();
+                        ?>
+                        <?php
+                      else :
+                        esc_html_e('No testimonials in the diving taxonomy!', 'text-domain');
+                      endif;
+                      ?>
+                  </div>
+                  <!--mobile-blog-carousel-->
+                      <div class="mobile-blog-carousel">
+                    <div class="medium-blog-row">    
+                        <div class="owl-carousel owl-theme" id="medium-blog-carousel">
+                      <?php
+                      $args = array(
+                          'post_status' => 'publish',
+                          'posts_per_page' => 3,
+                          'post__not_in' => array(get_the_id())
+                      );
+                      $testimonials = new WP_Query($args);
+                      if ($testimonials->have_posts()) :
+                        ?>
+                        <?php
+                        while ($testimonials->have_posts()) :
+                          $testimonials->the_post();
+                          ?>
+                          <div class="medium-blog-col blog-col-section">
+                              <a href="<?php the_permalink(); ?>" class="card-link item">
+                                  <div class="card">									
+                                      <img class="card-img-top" src="<?php the_post_thumbnail_url($size); ?>" alt="Card image cap">
+                                      <div class="card-body">
+                                          <h5 class="card-title"><?php the_title(); ?></h5>
+                                          <div class="read-story-btn-wrapper">              
+                                              <span>READ STORY</span>
+                                          </div>                                         
+                                      </div>							
+                                  </div>
+                              </a>
+                          </div>
+                          <?php
+                        endwhile;
+                        wp_reset_postdata();
+                        ?>
+                        <?php
+                      else :
+                        esc_html_e('No testimonials in the diving taxonomy!', 'text-domain');
+                      endif;
+                      ?>
+                        
+                     </div>   
+                  </div>
+                  </div>
+              </div>            
+          </div>
+      </div>
+  </main>
+  <?php
 endwhile;
 wp_reset_query();
 get_footer();
